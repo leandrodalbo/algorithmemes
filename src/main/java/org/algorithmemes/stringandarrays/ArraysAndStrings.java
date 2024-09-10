6230538ef1cb752db0338b792626f98efe949709
leandrodalbo <leandro.e.dal.bo@gmail.com>
2024-09-10 20:48:48 +0100

    string permutations

--- src/main/java/org/algorithmemes/stringandarrays/ArraysAndStrings.java
+++ src/main/java/org/algorithmemes/stringandarrays/ArraysAndStrings.java
@@ -1,9 +1,26 @@
 package org.algorithmemes.stringandarrays;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ArraysAndStrings {
+
+    public static boolean isPermutation(String a, String b) {
+
+        if (a.length() != b.length())
+            return false;
+        int[] allChars = new int[128];
+
+        for (int i = 0; i < a.length(); i++)
+            allChars[a.charAt(i)]++;
+        for (int i = 0; i < a.length(); i++) {
+            allChars[b.charAt(i)]--;
+            if (allChars[b.charAt(i)] < 0)
+                return false;
+        }
+        return true;
+    }
 
     public static boolean isAllUniqueChars(String s) {
         Map<Character, Integer> map = new HashMap<>();
--- src/test/java/org/algorithmemes/stringandarrays/ArraysAndStringsTest.java
+++ src/test/java/org/algorithmemes/stringandarrays/ArraysAndStringsTest.java
@@ -7,6 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ArraysAndStringsTest {
 
     @Test
+    public void shouldCheckForPermutation() {
+        assertThat(ArraysAndStrings.isPermutation("dog", "god")).isTrue();
+        assertThat(ArraysAndStrings.isPermutation("dog", " god")).isFalse();
+        assertThat(ArraysAndStrings.isPermutation("dog", "cat")).isFalse();
+    }
+
+    @Test
     public void shouldCheckForUniqueCharacters() {
         assertThat(ArraysAndStrings.isAllUniqueChars("abca")).isFalse();
         assertThat(ArraysAndStrings.isAllUniqueChars("abbc")).isFalse();
