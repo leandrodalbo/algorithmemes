6d67944d823425799f299b75ec2015800de55a66
leandrodalbo <leandro.e.dal.bo@gmail.com>
2024-09-10 12:15:04 +0100

    All characters are unique + tdd

--- src/main/java/org/algorithmemes/stringandarrays/ArraysAndStrings.java
+++ src/main/java/org/algorithmemes/stringandarrays/ArraysAndStrings.java
@@ -1,4 +1,23 @@
 package org.algorithmemes.stringandarrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ArraysAndStrings {
+
+    public static boolean isAllUniqueChars(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            Character c = s.charAt(i);
+            Integer count = map.get(c);
+
+            if (count == null)
+                map.put(c, 1);
+            else
+                return false;
+        }
+
+        return true;
+    }
 }
--- src/test/java/org/algorithmemes/stringandarrays/ArraysAndStringsTest.java
+++ src/test/java/org/algorithmemes/stringandarrays/ArraysAndStringsTest.java
@@ -1,4 +1,15 @@
 package org.algorithmemes.stringandarrays;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class ArraysAndStringsTest {
+
+    @Test
+    public void shouldCheckForUniqueCharacters() {
+        assertThat(ArraysAndStrings.isAllUniqueChars("abca")).isFalse();
+        assertThat(ArraysAndStrings.isAllUniqueChars("abbc")).isFalse();
+        assertThat(ArraysAndStrings.isAllUniqueChars("abc")).isTrue();
+    }
 }
